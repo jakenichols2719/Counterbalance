7364f48d33267067a1ffbe534498dcf84d79b970
jake <jnichols2719@protonmail.com>
2022-02-18 20:30:45 -0700

    Updated index.js to reflect prev changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 */
 
 app.get("/", (req, res) => {
-    res.render("index", {title: "Home"});
+    res.render("example-index", {title: "Home"});
 });
 
 /**
